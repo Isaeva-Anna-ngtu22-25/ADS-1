d66633048db79de7500a69900d0aad453c03dd2a
Isaeva Anna <116427105+Isaeva-Anna-ngtu22-25@users.noreply.github.com>
2023-02-25 21:26:15 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -3,8 +3,7 @@
 #include "alg.h"
 
 
-bool checkPrime(uint64_t ch) 
-{
+bool checkPrime(uint64_t ch) {
     if (ch <= 1)
         return false;
     if (ch <= 3)
@@ -17,16 +16,13 @@ bool checkPrime(uint64_t ch)
     return true;
 }
 
-uint64_t nPrime(uint64_t n)
-{
-    for (int x = 1; x++;) 
-    {
+uint64_t nPrime(uint64_t n) {
+    for (int x = 1; x++;) {
         if (int i = 0; i < n) {
             if (checkPrime(x)) {
                 i++;
             }
-        }
-        else if (i == n) {
+        } else if (i == n) {
             return x;
         }
     }
@@ -40,8 +36,7 @@ uint64_t nextPrime(uint64_t value) {
     return value;
 }
 
-uint64_t sumPrime(uint64_t hbound) 
-{
+uint64_t sumPrime(uint64_t hbound) {
     int sum = 0;
     for (int x = 2; x < hbound; x++) {
         if (checkPrime(x))
