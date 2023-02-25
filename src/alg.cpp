7eb339b06fc66a67830d574521cc68ad541b3eff
Isaeva Anna <116427105+Isaeva-Anna-ngtu22-25@users.noreply.github.com>
2023-02-25 22:42:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,41 +6,37 @@
 bool checkPrime(uint64_t ch) {
     if (ch <= 1)
         return false;
-    if (ch <= 3)
+    if (ch == 2 || ch == 3)
         return true;
     if (ch % 2 == 0)
         return false;
-    for (int x = 5; x * x <= ch; x += 2)
+    for (uint64_t x = 5; x * x <= ch; x += 2)
         if (ch % x == 0)
             return false;
     return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    for (int x = 1; x++;) {
-        if (int i = 0; i < n) {
-            if (checkPrime(x)) {
-                i++;
-            }
-        } else if (i == n) {
-            return x;
-        }
-    }    
+    uint64_t nP = 0;
+    uint64_t nom = 0;
+    for (uint64_t i = 2; nom < n; i++)
+        if (checkPrime(i))
+            nP = i;
+            nom++;
+     return nP;
 }
 
-uint64_t nextPrime(uint64_t value) {
-    while (!checkPrime(value)) {
-        value++;
-    }
-    return value;
+uint64_t nextPrime(uint64_t c) {
+    uint16_t z = c + 1;
+    while (!checkPrime(z))
+        z++;
+    return z;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    int sum = 0;
-    for (int x = 2; x < hbound; x++) {
-        if (checkPrime(x)) {
+    uint64_t sum = 0;
+    for (uint64_t x = 2; x < hbound; x++)
+        if (checkPrime(x))
             sum+=x;
-        }
-    }
     return sum;
 }
